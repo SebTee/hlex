ea8e23e5ab59551020586d070782389082f5c836
seb <seb.tom.tee@gmail.com>
2023-04-13 21:49:36 +0100

    Setup tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,23 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+import Test.HUnit
+import Hlex
+
+main :: IO Counts
+main = runTestTT tests
+
+data Token = Ident String
+
+grammar :: Grammar Token
+grammar = [ Tokenize "[a]+" Ident
+          , Skip "[\n]+"
+          ]
+
+lexer :: Lexer Token
+lexer = hlex grammar
+
+tests :: Test
+tests = TestList [exception]
+
+exception :: Test
+exception = TestCase $ case lexer "aaaa\n\naaaaa\naaa\naaabbbaa\naaaaa" of
+    Right _ -> assertFailure "Successfully parsed"
+    Left err -> assertEqual "Incorrect exception" err $ LexError 5 4 "bbb"
