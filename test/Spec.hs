f1e2a21b38c081303dc9a8c3ece7de9ff4238cdf
seb <seb.tom.tee@gmail.com>
2023-04-22 17:30:27 +0100

    Setup CI

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,9 +1,14 @@
+import System.Exit
 import Test.HUnit
 import Exceptions
 import Successes
 
-main :: IO Counts
-main = runTestTT tests
+main :: IO ()
+main = do
+  results <- runTestTT tests
+  case results of
+    Counts {errors = 0, failures = 0} -> exitSuccess
+    _ -> exitFailure
 
 tests :: Test
 tests = TestList [TestLabel "Successes" successes, TestLabel "Exceptions" exceptions]
